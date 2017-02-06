f0c4e7d179179c18c19ee46dd2813b426ab7098b
juvekeeper41 <josb6385@740s-pl-114.local>
2017-02-06 09:02:37 -0700

    Created the framework for the mega project.

---
+++ MegaProject/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  MegaProject
+//
+//  Created by Osborne, Jacob on 2/6/17.
+//  Copyright © 2017 Osborne, Jacob. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ MegaProject/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  MegaProject
+//
+//  Created by Osborne, Jacob on 2/6/17.
+//  Copyright © 2017 Osborne, Jacob. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+#endif /* Controller_hpp */
