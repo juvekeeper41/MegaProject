6853a9ec73bacd30570bdbb83fcd291b41f54aef
juvekeeper41 <josb6385@740s-pl-114.local>
2017-03-01 09:16:56 -0700

    Began the creation of the DoublyLinkedList class.

---
+++ MegaProject/TypedDataSample/DoublyLinkedList.h
@@ -0,0 +1,38 @@
+//
+//  DoublyLinkedList.h
+//  MegaProject
+//
+//  Created by Osborne, Jacob on 3/1/17.
+//  Copyright © 2017 Osborne, Jacob. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+private:
+    BiDirectionalNode<Type> * front;
+    BiDirectionalNode<Type> * end;
+    int size;
+public:
+    virtual void add(Type value) = 0;
+    virtual Type remove(int index) = 0;
+    DoublyLinkedList()
+    {
+        size = 0;
+        front = nullptr;
+        end = nullptr;
+    }
+    virtual ~DoublyLinkedList() = 0;
+    
+    int getSize() const;
+    BiDirectionalNode<Type> * getFront() const;
+    BiDirectionalNode<Type> * getEnd() const;
+};
+
+
+#endif /* DoublyLinkedList_h */
