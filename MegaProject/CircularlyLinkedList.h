3f77d8bd3b28ac923ed6f5e10f006f7e3cc94e14
juvekeeper41 <josb6385@740s-pl-114.local>
2017-03-01 09:55:43 -0700

    Edited the Mega UML and created the proper classes inside of the TypedDataSample package.

---
+++ MegaProject/CircularlyLinkedList.h
@@ -0,0 +1,13 @@
+//
+//  CircularlyLinkedList.h
+//  MegaProject
+//
+//  Created by Osborne, Jacob on 3/1/17.
+//  Copyright © 2017 Osborne, Jacob. All rights reserved.
+//
+
+#ifndef CircularlyLinkedList_h
+#define CircularlyLinkedList_h
+
+
+#endif /* CircularlyLinkedList_h */
---
+++ MegaProject/Queue.h
@@ -0,0 +1,13 @@
+//
+//  Queue.h
+//  MegaProject
+//
+//  Created by Osborne, Jacob on 3/1/17.
+//  Copyright © 2017 Osborne, Jacob. All rights reserved.
+//
+
+#ifndef Queue_h
+#define Queue_h
+
+
+#endif /* Queue_h */
