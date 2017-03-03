6b3a00fc98379efc1eebdf10772573c1adf11167
juvekeeper41 <josb6385@740s-pl-114.local>
2017-03-03 09:54:30 -0700

    Created essential methods inside of the BiDirectionalNode class.

--- MegaProject/Model/BiDirectionalNode.h
+++ MegaProject/Model/BiDirectionalNode.h
@@ -13,34 +13,48 @@ template <class Type>
 class BiDirectionalNode : public Node<Type>
 {
 private:
-    Type data;
+//    Type data; removed as the data member will be inherited from Node<Type>
     BiDirectionalNode<Type> * nextPointer;
-    BiDirectionalNode<Type> * previousPointer;
+    BiDirectionalNode<Type> * previous;
 public:
     BiDirectionalNode();
     BiDirectionalNode(Type data);
     BiDirectionalNode(Type Data, BiDirectionalNode<Type> * previous, BiDirectionalNode<Type> * next);
     
-    Type getNodeData();
+//    Type getNodeData();
     BiDirectionalNode<Type> * getNextPointer();
     BiDirectionalNode<Type> * getPreviousPointer();
     
-    void setNodeData(Type data);
+//    void setNodeData(Type data);
     void setNextPointer(BiDirectionalNode<Type> * next);
     void setPreviousPointer(BiDirectionalNode<Type> * previous);
 };
 
 template <class Type>
-BiDirectionalNode :: BiDirectionalNode() : Node()
+BiDirectionalNode :: BiDirectionalNode() : Node<Type>()
 {
-    
+    this->nextPointer = nullptr;
+    this->previous = nullptr;
 }
 
 template <class Type>
 BiDirectionalNode :: BiDirectionalNode(Type data) : Node(data)
 {
-    
+    this->nextPointer = nullptr;
+    this->previous = nullptr;
 }
 
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode(Type data, BiDirectionalNode<Type> * previous, BiDirectionalNode<Type> * next) : Node(data, next)
+{
+    this->nextPointer = next;
+    this->previous = previous;
+}
+
+template <class Type>
+void BiDirectionalNode<Type> :: getPreviousPointer(BiDirectionalNode<Type> * previous)
+{
+    this->previous = previous;
+}
 
 #endif /* BiDirectionalNode_h */
