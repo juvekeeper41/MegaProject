167cecd1b0481c822012845611f4302ce0e6debf
juvekeeper41 <josb6385@740s-pl-114.local>
2017-03-07 09:55:30 -0700

    Created methods in DoublyLinkedList.

--- MegaProject/InheritsFromDoublyLinked/DoubleList.h
+++ MegaProject/InheritsFromDoublyLinked/DoubleList.h
@@ -9,5 +9,58 @@
 #ifndef DoubleList_h
 #define DoubleList_h
 
+template <class Type>
+class DoubleList : public DoublyLinkedList<Type>
+{
+private:
+public:
+    DoubleList();
+    ~DoubleList();
+    void add(Type data);
+    Type remove(int index);
+    void addAtIndexFast(int index, Type value);
+    void addAtIndex(int index, Type value);
+    Type getFromIndex(int index);
+    Type getFromIndexFast(int index);
+};
+
+template <class Type>
+DoubleList<Type> :: DoubleList() : DoubleList() : DoublyLinkedList<Type>()
+{
+    
+}
+
+template <class Type>
+DoubleList<Type> :: ~DoubleList()
+{
+    
+}
+
+template <class Type>
+Type DoubleList<Type> :: getFromindexFast(int index)
+{
+    assert(index >= 0 && index < this->getSize());
+    Type valueAtIndex;
+    BiDirectionalNode<Type> * reference;
+    if(index < this->getSize() / 2)
+    {
+        reference = this->getFront();
+        for(int position = 0; position < index; position++)
+        {
+            reference = reference->getNextPointer();
+        }
+    }
+    else
+    {
+        reference = this->getEnd();
+        for(int position = this->getSize() - 1; position > index; position--)
+        {
+            reference = reference->getPreviousPointer();
+        }
+    }
+    
+    valueAtIndex = reference->getNodeData();
+    return valueAtIndex;
+}
 
 #endif /* DoubleList_h */
