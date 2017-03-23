af663ab3f2de6b489cf646a780106dbc0b589e2a
juvekeeper41 <josb6385@740s-pl-114.local>
2017-03-23 09:17:01 -0600

    Created the Array class inside of model

---
+++ MegaProject/Model/Array.h
@@ -0,0 +1,15 @@
+//
+//  Array.h
+//  MegaProject
+//
+//  Created by Osborne, Jacob on 3/23/17.
+//  Copyright © 2017 Osborne, Jacob. All rights reserved.
+//
+
+#ifndef Array_h
+#define Array_h
+
+
+
+
+#endif /* Array_h */
