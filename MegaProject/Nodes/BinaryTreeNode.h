7b72146d047b461b0027c630387c2b28573d84ec
juvekeeper41 <josb6385@740s-pl-114.local>
2017-04-11 09:25:52 -0600

    Worked on Tree and BinaryTreeNode.

--- MegaProject/Nodes/BinaryTreeNode.h
+++ MegaProject/Nodes/BinaryTreeNode.h
@@ -9,5 +9,35 @@
 #ifndef BinaryTreeNode_h
 #define BinaryTreeNode_h
 
+#include "Tree.hpp"
+#include "BinarySearchTreeNode.hpp"
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+private:
+    BinarySearchTreeNode<Type> * root;
+    int calculateSize(BinarySearchTreeNode<Type> * root);
+    void inOrderTraversal(BinarySearchTreenode<Type> * start);
+    void preOrderTraversal(BinarySearchTreeNode<Type> * preStart);
+    void postOrderTraversal(BinarySearchTreeNode<Type> * postStart);
+    
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinarySearchTreeNode<Type> * getRoot();
+    void setRoot(BinarySearchTreeNode<Type> * root);
+    
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+};
+
+template <class Type>
+BinarySearchTree<Type> :: BinarySearchTree() : Tree<Type>()
+{
+    this->root = nullptr;
+}
 
 #endif /* BinaryTreeNode_h */
