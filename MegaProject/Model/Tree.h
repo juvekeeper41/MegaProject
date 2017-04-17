584c5748464e5baa230c709672ff3bb0e2a1179a
juvekeeper41 <josb6385@740s-pl-114.local>
2017-04-17 07:39:31 -0600

    Made the entire class virtual

--- MegaProject/Model/Tree.h
+++ MegaProject/Model/Tree.h
@@ -15,84 +15,23 @@ template <class Type>
 class Tree
 {
 private:
-    int height;
-    int size;
-    bool complete;
-    bool balanced;
-    
-public:
-    Tree();
-    virtual ~Tree();
-    virtual void printToFile() = 0;
+    //Traversals
     virtual void inOrderTraversal() = 0;
     virtual void postOrderTraversal() = 0;
     virtual void preOrderTraversal() = 0;
+    //Informational Methods
+    virtual int getHeight() = 0;
+    virtual int getSize() = 0;
+    virtual bool isComplete() = 0;
+    virtual bool isBalanced() = 0;
+    //Data Methods
+    virtual bool contains(Type value) = 0;
+    virtual bool insert(Type itemToInsert) = 0;
+    virtual bool remove(Type value) = 0;
+public:
     
-    int getHeight() const;
-    int getSize() const;
-    bool isComplete() const;
-    bool isBalanced() const;
-    
-    void setHeight(int height);
-    void setSize(int size);
-    void setComplete(bool complete);
-    void setBalanced(bool balanced);
 };
 
-template <class Type>
-Tree<Type> :: Tree()
-{
-    height = 0;
-    size = 0;
-    balanced = false;
-    complete = false;
-}
 
-template <class Type>
-int Tree<Type> :: getHeight() const
-{
-    return this->height;
-}
-
-template <class Type>
-int Tree<Type> :: getSize() const
-{
-    return this->size;
-}
-
-template <class Type>
-bool Tree<Type> :: isComplete() const
-{
-    return this->complete;
-}
-
-template <class Type>
-bool Tree<Type> :: isBalanced() const
-{
-    return this->balanced;
-}
-
-template <class Type>
-void Tree<Type> :: setHeight(int height)
-{
-    this->height = height;
-}
-
-template <class Type>
-void Tree<Type> :: setSize(int size)
-{
-    this->size = size;
-}
-
-template <class Type>
-void Tree<Type> :: setComplete(bool complete)
-{
-    this->complete = complete;
-}
-template <class Type>
-void Tree<Type> :: setBalanced(bool balanced)
-{
-    this->balanced = balanced;
-}
 
 #endif /* Tree_h */
